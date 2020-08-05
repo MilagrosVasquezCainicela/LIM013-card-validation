2bbb6b95fc3b7681dda96d4f09ca50ffd17ff773
MilagrosVásquezCainicela <milagrosvasquezcainicelaunmsm@gmail.com>
2020-08-05 12:24:31 -0500

    validator funciona, aún falta index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-import validator from './validator.js';
 
-console.log(validator);
+import validator from './validator.js';
+//console.log(validator.isValid("4083952015263"));
+
+// llamando al elemento con ese id
+let send = document.getElementById('send')
+let card = document.getElementById('inputNumero');
+// asigno un observador
+send.addEventListener('click', function(){
+    event.preventDefault();
+    let numero=card.value;
+   let resultado=validator.isValid(numero); 
+   let mascara=validator.maskify(numero); 
+   if(resultado==true){
+    numero= mascara;
+   document.getElementById("resultado").textContent= mascara + " Tarjeta Válida";
+  
+} else{ 
+       document.getElementById("resultado").textContent="Tarjeta Inválida"; 
+
+   }
+    
+
+})
+
+// }) ;
+//* Seleccionar el año generado dinamicamente. 
+const yearActual =(new Date()).getFullYear();
+console.log(yearActual)
+let select = document.getElementById("selectYear")
+for(let i = yearActual; i >= 2011; i--){
+    let opcion = document.createElement("option")
+    opcion.text= i;
+    opcion.value = i;
+    select.add(opcion);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,49 @@
 const validator = {
-  // ...
-};
+  isValid:(numero)=>{
+    let rev ='';
+    for(let i = numero.length -1; i >=0; i--){
+      rev += numero[i]
+    }
+    const array = Array.from(rev).map(Number);
+    let impar=[] ;
+    for(let i=0; i < array.length; i++){
+      if(i%2!==0){
+          impar.push(array[i]*2);
+      }else{
+          impar.push(array[i]*1);
+    }
+    }
+    let list = [];
+    for(let i=0; i<impar.length; i++){
+        if(impar[i]>=10){
+           list.push((impar[i]-10)+1);
+            }else{
+                list.push(impar[i]*1);
+            }
+    }
+    const total = list.reduce((sum, current) => sum + current, 0);
+    if(total%10 ===0 ){
+      return true;
+    } else {
+      return false; 
+    }
+  },
+
+
+  maskify:(numero)=>{
+    let maskify2 = '';
+        for(let i=0; i<numero.length; i++){
+            if(i<=numero.length-5){
+                maskify2 = maskify2 + '*';
+            }else{
+                maskify2 = maskify2 + numero[i];
+            }
+        }
+
+//console.log(maskify2)       
+ return maskify2
+  }
+  
+}
 
 export default validator;
